45f94e26e43002819e56d8fc6036df6477c2b12d
JohN100x1 <JohN100x1@hotmail.co.uk>
2022-04-13 19:46:43 +0100

    test: added load_json test

---
+++ tests/io/test_loaders.py
@@ -0,0 +1,23 @@
+from json import JSONDecodeError
+from pathlib import Path
+
+import pytest
+
+from reddit_analysis.io.loaders import load_json
+
+
+class TestLoadJson:
+    """Test load_json."""
+
+    def test_success(self):
+        path_json = Path(__file__).parent / "fake_file.json"
+        assert load_json(path_json) == {"foo": "bar"}
+
+    def test_file_not_found(self):
+        with pytest.raises(FileNotFoundError):
+            assert load_json(Path("foo.json"))
+
+    def test_invalid_file(self):
+        path_json = Path(__file__)
+        with pytest.raises(JSONDecodeError):
+            assert load_json(path_json)
